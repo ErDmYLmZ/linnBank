f1b92c8cc2763466fc0d88dd9337a4d54b55664b
Enes Kocak <enes51kocak@gmail.com>
2021-11-19 16:30:46 +0300

    reports gitignore

---
+++ src/test/java/com/linnbank/runner/Runner.java
@@ -0,0 +1,24 @@
+package com.linnbank.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:reports/default-cucumber-reports.html",
+                "json:reports/json-reports/cucumber.json",
+                "junit:reports/xml-report/cucumber.xml",
+                "rerun:reports/failedRerun.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        features = "./src/test/resources/features",
+        glue = ".stepdefinitions",
+        tags = "@selenium_grid",
+        dryRun = false,
+        monochrome = true
+)
+public class Runner {
+}
--- src/test/java/com/linnbank/runner/RunnerTest.java
+++
@@ -1,4 +0,0 @@
-package com.linnbank.runner;
-
-public class RunnerTest {
-}
