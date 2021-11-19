828754adfd393831fdfe56e8ded22839f1ab2e66
Enes Kocak <enes51kocak@gmail.com>
2021-11-19 23:27:41 +0300

    pages

--- src/test/java/com/linnbank/pages/ex_page.java
+++
@@ -1,9 +0,0 @@
-package com.linnbank.pages;
-
-public class ex_page {
-    public static void main(String[] args) {
-        System.out.println("zafer");
-        System.out.println("mehmet");
-
-    }
-}
