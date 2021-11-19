a7b960de614783b139117a23e3e01e2ed1534861
Enes Kocak <enes51kocak@gmail.com>
2021-11-19 23:19:09 +0300

    pages template prepared

--- src/test/java/com/linnbank/pages/MyAccountsPage.java
+++ src/test/java/com/linnbank/pages/MyAccountsPage.java
@@ -1,4 +1,12 @@
 package com.linnbank.pages;
 
+import com.linnbank.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class MyAccountsPage {
+
+    public MyAccountsPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
 }
--- src/test/java/com/linnbank/pages/TransferMoneyPage.java
+++ src/test/java/com/linnbank/pages/TransferMoneyPage.java
@@ -1,4 +1,12 @@
 package com.linnbank.pages;
 
+import com.linnbank.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class TransferMoneyPage {
+
+    public TransferMoneyPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
 }
