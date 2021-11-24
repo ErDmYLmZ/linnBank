f0f1b11537e58fc3d3d2dc92c8e0028a6cb327a9
Emir Karslı <ekarsli61@gmail.com>
2021-11-24 18:01:16 +0300

    us003 feature file

--- src/test/java/com/linnbank/pages/RegisterPage.java
+++ src/test/java/com/linnbank/pages/RegisterPage.java
@@ -1,10 +1,46 @@
 package com.linnbank.pages;
 
 import com.linnbank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegisterPage {
+
     public RegisterPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(id = "ssn")
+    public WebElement ssnInput;
+
+    @FindBy(id = "firstname")
+    public WebElement firstNameInput;
+
+    @FindBy(id = "lastname")
+    public WebElement lastNameInput;
+
+    @FindBy(id = "address")
+    public WebElement addressInput;
+
+    @FindBy(id = "mobilephone")
+    public WebElement phoneInput;
+
+    @FindBy(id = "username")
+    public WebElement usernameInput;
+
+    @FindBy(id = "email")
+    public WebElement emailInput;
+
+    @FindBy(id = "firstPassword")
+    public WebElement firstPasswordInput;
+
+    @FindBy(id = "strengthBar")
+    public WebElement passwordStrengthBar;
+
+    @FindBy(id = "secondPassword")
+    public WebElement secondPasswordInput;
+
+    @FindBy(id = "register-submit")
+    public WebElement submitRegisterButton;
 }
