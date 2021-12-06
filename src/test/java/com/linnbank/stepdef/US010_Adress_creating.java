d4ca512309013590f04f23ab8339117772359d36
Mehmetty77 <avukatmehmettoy@gmail.com>
2021-12-06 21:54:36 +0100

    feature_error_fixed

--- src/test/java/com/linnbank/stepdef/US010_Adress_creating.java
+++ src/test/java/com/linnbank/stepdef/US010_Adress_creating.java
@@ -1,23 +1,29 @@
 package com.linnbank.stepdef;
 
+import com.github.javafaker.Faker;
 import com.linnbank.pages.MainPage;
 import com.linnbank.pages.ManageCustomersPage;
 import com.linnbank.pages.SignInPage;
-import com.linnbank.utilities.BrowserUtils;
-import com.linnbank.utilities.ConfigReader;
-import com.linnbank.utilities.Driver;
-import com.linnbank.utilities.JSUtils;
+import com.linnbank.pojos.Customer;
+import com.linnbank.utilities.*;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.it.Ma;
+import org.kohsuke.rngom.parse.host.Base;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.Locale;
 
 public class US010_Adress_creating {
 
-    ManageCustomersPage createCustomer;
-    MainPage mainPage;
-    SignInPage signInPage;
+    ManageCustomersPage manageCustomersPage=new ManageCustomersPage();
+    MainPage mainPage=new MainPage();
+    SignInPage signInPage=new SignInPage();
+    Customer customer=new Customer();
+    Faker faker=new Faker(new Locale("en-US"));
 
 
 
@@ -45,8 +51,8 @@ public class US010_Adress_creating {
 
     @And("click create new customer")
     public void click_create_new_customer() {
-        createCustomer=new ManageCustomersPage();
-        JSUtils.clickElementByJS(createCustomer.createNewCustomerButton);
+        manageCustomersPage=new ManageCustomersPage();
+        JSUtils.clickElementByJS(manageCustomersPage.createNewCustomerButton);
         BrowserUtils.wait(1);
 
 
@@ -55,55 +61,97 @@ public class US010_Adress_creating {
     @When("type new customers all necessary credentials")
     public void type_new_customers_all_necessary_credentials() {
 
+        manageCustomersPage.firstName.sendKeys(faker.name().firstName());
+        manageCustomersPage.lastName.sendKeys(faker.name().lastName());
+        manageCustomersPage.middleInitial.sendKeys(faker.name().name());
+        manageCustomersPage.email.sendKeys(faker.internet().emailAddress());
+        BrowserUtils.wait(1);
+        manageCustomersPage.mobilePhoneNumber.sendKeys("123-456-7893");
+        BrowserUtils.wait(1);
+        manageCustomersPage.phoneNumber.sendKeys("213-456-7893");
+        manageCustomersPage.zipCode.sendKeys(faker.address().zipCode());
+        customer.setSsn(faker.idNumber().ssnValid());
+        BrowserUtils.wait(2);
+        manageCustomersPage.ssn.sendKeys(customer.getSsn());
+
+
+
+//
+//
+//        manageCustomersPage.createDate.clear();
+//        manageCustomersPage.createDate.sendKeys(mapData.get(0).get("CreateDate"), Keys.RIGHT);
+//        manageCustomersPage.createDate.sendKeys(mapData.get(0).get("CreateHour"));
+//
+//        Select select = new Select(manageCustomersPage.countryDropdown);
+//        ReusableMethods.selectRandomTextFromDropdown(select);
+
     }
 
     @When("type city textbox")
     public void type_city_textbox() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtils.wait(1);
+        manageCustomersPage.city.sendKeys(faker.address().city());
     }
 
     @When("type state textbox")
     public void type_state_textbox() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtils.wait(1);
+        manageCustomersPage.state.sendKeys(faker.address().state());
     }
 
     @When("select from country dropdown")
     public void select_from_country_dropdown() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Select select = new Select(manageCustomersPage.countryDropdown);
+
+        select.selectByVisibleText("USA");
+
+        //ReusableMethods.selectRandomTextFromDropdown(select);
+    }
+    @When("select empty option from country dropdown")
+    public void select_empty_option_from_country_dropdown() {
+        Select select = new Select(manageCustomersPage.countryDropdown);
+        select.getFirstSelectedOption();
     }
 
     @When("type adress textbox {string}")
-    public void type_adress_textbox(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void type_adress_textbox(String address) {
+        BrowserUtils.wait(1);
+        manageCustomersPage.adress.sendKeys(address);
+
     }
 
     @Then("verify that Address isn't created")
     public void verify_that_address_isn_t_created() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtils.wait(2);
+        manageCustomersPage.saveButton.click();
+        BrowserUtils.wait(10);
+        System.out.println(customer.getSsn());
+
+
     }
 
     @When("type customer adress textbox")
     public void type_customer_adress_textbox() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtils.wait(1);
+        manageCustomersPage.adress.sendKeys(faker.address().streetAddress());
+        BrowserUtils.wait(1);
+        System.out.println(faker.address().streetAddress());
     }
 
     @When("type city textbox {string}")
-    public void type_city_textbox(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void type_city_textbox(String invalidCity) {
+        BrowserUtils.wait(1);
+        manageCustomersPage.city.sendKeys(invalidCity);
+
     }
 
     @When("type state textbox {string}")
-    public void type_state_textbox(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void type_state_textbox(String invalidState) {
+        BrowserUtils.wait(1);
+        manageCustomersPage.state.sendKeys(invalidState);
+
     }
+
 
 
 
